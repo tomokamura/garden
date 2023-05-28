fb1e700927b2cc252d10c6992d9557cb9e6f8c36
tomokamura <gangcunzx@gmail.com>
2023-05-28 20:45:23 +0900

    😭try...

--- src/pages/api/dog.json.js
+++ src/pages/api/dog.json.js
@@ -1,7 +1,7 @@
 export const get = async ({ params, request }) => {
   let resData;
 
-  await fetch(`https://dog.ceo/api/breeds/image/random`, {cache: 'no-cache'})
+  await fetch(`https://dog.ceo/api/breeds/image/random`, {cache: 'no-store'})
   .then((response) => response.json())
   .then((data) => {
     resData = data;
