f550d1a1b837d69990999984e7659bc261f2b9e2
tomokamura <gangcunzx@gmail.com>
2023-05-28 20:58:44 +0900

    onegai

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true,
+  },
   integrations: [solidJs(), tailwind()]
 });
