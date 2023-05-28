db50244abe5063572aa9cd93e0626c8831555363
tomokamura <gangcunzx@gmail.com>
2023-05-28 21:01:01 +0900

    onegai

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   experimental: {
-    hybridOutput: true,
+    hybridOutput: true
   },
-  integrations: [solidJs(), tailwind()]
+  integrations: [solidJs(), tailwind()],
+  adapter: netlify()
 });
